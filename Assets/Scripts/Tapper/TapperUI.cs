b7a4bde1a438da827f5d8fe6b3c40bbb3272eb69
mwalden <tont0r@hotmail.com>
2019-02-21 16:09:31 -0500

    adding ui

---
+++ Assets/Scripts/Tapper/TapperUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapperUI : MonoBehaviour
+{
+    public Text message;
+    public Image image;
+    public Sprite[] images;
+    public string[] messages;
+    public int currentMessage;
+    public float timeToDisplay;
+
+    public void close()
+    {
+
+    }
+
+    void showUI()
+    {
+        image.sprite = images[currentMessage];
+        message.text = messages[currentMessage];
+        currentMessage++;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
